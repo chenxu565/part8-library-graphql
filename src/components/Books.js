4d38b72966925aebc86f7dda37af52085c6850c8
Chen Xu <cxu.self@gmail.com>
2024-04-01 18:45:24 +0300

    8.19 and 8.21 Books by genre
    Complete the application to filter the book list by genre.

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,24 +1,62 @@
 import { useQuery } from '@apollo/client';
-
-import { GET_ALL_BOOKS } from '../queries';
+import { useState } from 'react';
+import { GET_ALL_BOOKS, GET_ALL_GENRES } from '../queries';
 
 const Books = (props) => {
-  const result = useQuery(GET_ALL_BOOKS);
+  const [genreTab, setGenreTab ] = useState("all genres")
+  const booksResult = useQuery(GET_ALL_BOOKS, 
+    {
+      skip: !props.show,
+      fetchPolicy: "cache-and-network",
+      variables: {
+        genre: genreTab==="all genres"? null: genreTab
+      }
+    });
 
-  if (result.loading) {
+  const genresResult = useQuery(GET_ALL_GENRES, 
+    {
+      skip: !props.show,
+      fetchPolicy: "cache-and-network"
+    }
+  )
+
+  if (booksResult.loading) {
     return <div>loading...</div>;
   }
-
+  
   if (!props.show) {
     return null
   }
+  const books = booksResult.data?.allBooks || [];
+  const uniqueGenres = genresResult.data?.allGenres || [];
+  const buttonGenres = [...uniqueGenres, 'all genres']
 
-  const books = result.data.allBooks;
+  const toggleGenre = (genre) => {
+    setGenreTab(genre);
+    booksResult.refetch(
+      {
+        genre: genre==="all genres"? null: genre
+      }
+    )
+  }
 
   return (
     <div>
       <h2>Books</h2>
-
+      {
+        buttonGenres.map(
+          (genre) =>
+          (
+            <button 
+              key={genre}
+              onClick={()=>toggleGenre(genre)}
+            >
+              {genre}
+            </button>
+          )
+        )
+      }
+      {genreTab==="all genres"? <p><b>All genres</b></p> : <p>in genre <b>{genreTab}</b></p>}
       <table>
         <tbody>
           <tr>
