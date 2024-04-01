b67a101ed5b9cba261ec9a85c9ae593f4fc05190
Chen Xu <cxu.self@gmail.com>
2024-04-01 21:50:32 +0300

    Add uniqueness check

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
+const uniqueValidator = require("mongoose-unique-validator");
+
 const schema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
     minlength: 3,
   },
   favoriteGenre: {
@@ -11,5 +14,7 @@ const schema = new mongoose.Schema({
     required: true,
   },
 });
+
+schema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("User", schema);
